2419ffa2de57ab4f4c37c0e785bb50a6e23e2a3e
mrsargent <sargent11235@gmail.com>
2024-04-26 21:47:53 +0000

    worked on validators and first minting script

---
+++ finalproject/Documentation/helper.sh
@@ -0,0 +1,29 @@
+#### Derive address from script
+cardano-cli address build --payment-script-file ./compiled/ak_typeddatum22.uplc --testnet-magic 2 --out-file ak_typeddatum22.addr
+
+#### PubKeyHash creation:
+## cardano-cli address key-hash --payment-verification-key-file benef1.vkey --out-file benef1.pkh
+
+#### Create protocol parameters file
+## cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.params
+
+#### Serializing contracts from blueprint for cardano CLI
+# aiken blueprint convert -m redeemer11 -v redeemer11 . > redeemer.uplc
+
+#### echo "{\"constructor\" : 0, \"fiels\": [{\"}]}
+
+## // compile validator into .uplc file
+#   aiken blueprint convert -m typed_validators -v ourWonderfullValidator . > ./testnet/compiled/ourWonderfullValidator.uplc
+# where typed_validators is the actual .ak file and ourWonderfullValidator is the validator inside that file
+## // another example 
+# aiken blueprint convert -m initialminter -v initialmint . > ./testnet/compiled/initialmint.uplc
+
+
+#   aiken blueprint address -m typed_validators -v ourWonderfullValidator . > ./testnet/compiled/ourWonderfullValidator.addr
+# where typed_validators is the actual .ak file and ourWonderfullValidator is the validator inside that file
+
+#  aiken minting policy comand
+#  aiken blueprint policy -m initialminter -v initialmint . > ./testnet/compiled/initialmint.pid
+
+#to view all available validators
+# aiken blueprint convert -m .  
---
+++ finalproject/testnet/shellscripts/initialmintLT.sh
@@ -0,0 +1,41 @@
+# this is the shell commands for the initial mint.  This is when Company A is going to mint 10000 Loyalty Tokens (LTs).
+# Company A can mint as many tokens as they want as many times as they want. The only thing that must happen is that 
+# company A must sign the transaction.  This prevents unauthorized people from minting
+
+utxoin1="4af8612f022bf17e33135c88dcdab347afceb5cf1e26db7cdaaeedbb49ad5e59#1"
+policyid="f3f453a3f38b18d74ce6b705f6abe3a3953445f3f75c9a2615e5300a" #$(cat ../../compiled/initialmint.pid)
+company_a="addr_test1vrpjhpjh5jpp8dtvmjaaytfh8em6wcu5exstl5ychded3ugznvwe3"
+output="12000000"
+tokenamount="10000"
+tokenname="4c6f79616c747920546f6b656e"   #$(echo -n "Loyalty Token" | xxd -ps | tr -d '\n')
+collateral_company_a="4af8612f022bf17e33135c88dcdab347afceb5cf1e26db7cdaaeedbb49ad5e59#0" 
+company_a_signerPKH="c32b8657a48213b56cdcbbd22d373e77a76394c9a0bfd098bb72d8f1"  
+txTimeInterval="--invalid-hereafter 10962786"
+PREVIEW="--testnet-magic 2"
+mintingScript="./../compiled/initialmint.uplc"
+
+cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.params
+
+cardano-cli transaction build \
+  --babbage-era \
+  $PREVIEW \
+  --tx-in $utxoin1 \
+  --required-signer-hash $company_a_signerPKH \
+  --tx-in-collateral $collateral_company_a \
+  --tx-out $company_a+$output+"$tokenamount $policyid.$tokenname" \
+  --change-address $company_a \
+  --mint "$tokenamount $policyid.$tokenname" \
+  --mint-script-file $mintingScript \
+  --mint-redeemer-file ./../values/empty.json \
+  --protocol-params-file protocol.params \
+  --out-file initialmint.unsigned
+
+cardano-cli transaction sign \
+    --tx-body-file initialmint.unsigned \
+    --signing-key-file ./../../Wallet/CompanyA.skey \
+    $PREVIEW \
+    --out-file initialmint.signed
+
+ cardano-cli transaction submit \
+    $PREVIEW \
+    --tx-file initialmint.signed
